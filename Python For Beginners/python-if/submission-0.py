6151ab1a8eba1ff0bd07cbcc326d7f53b8d8fb26
Ranjith Kumar R G <theranjith@gmail.com>
2026-09-14 17:18:29 +0530

    Add: python-if - submission-0

---
+++ Python For Beginners/python-if/submission-0.py
@@ -0,0 +1,11 @@
+account_balance=100
+
+def is_balance_low(balance: int) -> None:
+    if balance <= account_balance:
+        print("Warning: Low balance." )  
+
+
+# do not modify below this line
+is_balance_low(99)
+is_balance_low(100)
+is_balance_low(101)
